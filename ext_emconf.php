1b0cac96d8b7e5f29c89da8e7bffe89f9c15cbbd
Fabien Udriot <fabien@ecodev.ch>
2016-08-30 08:08:21 +0200

    Release 0.9.0 - https://github.com/fabarea/messenger/compare/0.9.0...master
    First public release on TER.

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,13 +5,13 @@ $EM_CONF[$_EXTKEY] = [
 	'description' => 'Send emails to a bunch of people. A message is wrapped in a template / layout that the User can freely edit in a BE module.',
 	'category' => 'plugin',
 	'author' => 'Fabien Udriot',
-	'author_email' => 'fabien.udriot@typo3.org',
+	'author_email' => 'fabien@ecodev.ch',
 	'state' => 'beta',
 	'version' => '0.9.0',
 	'constraints' => [
 		'depends' => [
 			'vidi' => '',
-			'typo3' => '6.2.0-7.6.99',
+			'typo3' => '7.6.0-7.6.99',
 		],
 		'conflicts' => [
 		],
