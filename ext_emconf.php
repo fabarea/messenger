ac8ea24243400beff6bbb37e8edae5d679c8a9ea
Fabien Udriot <fabien@omic.ch>
2018-03-29 09:45:53 +0200

    Set TYPO3 v8 as minimum requirement

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,21 +1,21 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-	'title' => 'Send messages to a bunch of people',
-	'description' => 'Send emails to a bunch of people. A message is wrapped in a template / layout that the User can freely edit in a BE module.',
-	'category' => 'plugin',
-	'author' => 'Fabien Udriot',
-	'author_email' => 'fabien@ecodev.ch',
-	'state' => 'beta',
-	'version' => '1.0.0-dev',
-	'constraints' => [
-		'depends' => [
-			'typo3' => '7.6.0-8.7.99',
-			'vidi' => '0.0.0-0.0.0',
-		],
-		'conflicts' => [
-		],
-		'suggests' => [
-		],
-	],
+    'title' => 'Send messages to a bunch of people',
+    'description' => 'Send emails to a bunch of people. A message is wrapped in a template / layout that the User can freely edit in a BE module.',
+    'category' => 'plugin',
+    'author' => 'Fabien Udriot',
+    'author_email' => 'fabien@ecodev.ch',
+    'state' => 'beta',
+    'version' => '1.0.0-dev',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7.0-8.7.99',
+            'vidi' => '0.0.0-0.0.0',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
 ];
