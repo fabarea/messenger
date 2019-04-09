f0c92eef4c37a32f51137dc0d56a83813121ba1c
Fabien Udriot <fabarea@users.noreply.github.com>
2019-04-09 10:42:21 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     ],
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-8.7.99',
+            'typo3' => '9.5.0-9.5.99',
             'vidi' => '2.6.0-0.0.0',
         ],
         'conflicts' => [
