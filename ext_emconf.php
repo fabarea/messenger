027b0ddc2ba4355e956bce5c6849e5abf25156c6
Fabien Udriot <fabien@omic.ch>
2019-06-10 22:03:23 +0200

    Release 2.1.0 - <https://github.com/fabarea/recipients_newsletter/compare/2.0.0...2.1.0>
    [FEATURE] Add configurable newsletter recipients data type

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,7 +7,7 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Fabien Udriot',
     'author_email' => 'fabien@ecodev.ch',
     'state' => 'beta',
-    'version' => '2.1.0-dev',
+    'version' => '2.1.0',
     'autoload' => [
         'psr-4' => ['Fab\\Messenger\\' => 'Classes']
     ],
