30435c77de6440b5125ddf44f701f5f64b7572a4
Fabien Udriot <fabien@omic.ch>
2019-06-28 16:54:31 +0200

    Release 2.1.1 - <https://github.com/fabarea/messenger/compare/2.1.0...2.1.1>
    [TASK] Enlarge scope of search to "recipient" in sent emails

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,7 +7,7 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Fabien Udriot',
     'author_email' => 'fabien@ecodev.ch',
     'state' => 'beta',
-    'version' => '2.2.0-dev',
+    'version' => '2.1.1',
     'autoload' => [
         'psr-4' => ['Fab\\Messenger\\' => 'Classes']
     ],
