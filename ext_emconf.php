6e8db1e0673f3ba558d00ee9f143157d325b89c5
Fabien Udriot <fabien@omic.ch>
2021-10-19 16:44:10 +0200

    Release 3.0.1 - <https://github.com/fabarea/messenger/compare/3.0.0...3.0.1>

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,7 +7,7 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Fabien Udriot',
     'author_email' => 'fabien@ecodev.ch',
     'state' => 'stable',
-    'version' => '3.0.0-dev',
+    'version' => '3.0.1',
     'autoload' => [
         'psr-4' => ['Fab\\Messenger\\' => 'Classes']
     ],
